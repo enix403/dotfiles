52116ac0a51e5e8aff83d88469c7b96c8e5ea497
enix403 <fog.code000@gmail.com>
2021-09-20 13:18:30 +0500

    Split i3 configuration into multiple files

---
+++ i3/README.md
@@ -0,0 +1,3 @@
+## Instructions
+
+Execute the `create_config.sh` command to generate to i3 `config` file from the split files in the `conf.d` directory
---
+++ i3/create_config.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# Get the absolute script path
+# https://stackoverflow.com/a/4774063
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+SCRIPTPATH=$(echo $SCRIPTPATH | sed -e 's#/$##')
+
+SCRIPTNAME=$(basename "$0")
+
+_confd_dir_name="conf.d"
+
+_confd_path="$SCRIPTPATH/$_confd_dir_name"
+_config_file="$SCRIPTPATH/config"
+
+rm -f $_config_file && touch $_config_file
+
+exe="$SCRIPTPATH/$SCRIPTNAME"
+echo -e "# ================ Required Keybindings and Setup ================\n" >> $_config_file
+echo "exec --no-startup-id $exe # Regenerate this config on every login" >> $_config_file
+echo "bindsym \$mod+F2 exec --no-startup-id $exe && i3-msg reload" >> $_config_file 
+echo "bindsym \$mod+Shift+F2 exec --no-startup-id $exe && i3-msg restart" >> $_config_file 
+
+for f in "$_confd_path"/*.conf;
+do
+    echo -e '\n' >> $_config_file
+    filename=$(basename "$f")
+    echo -e "# ================ $_confd_dir_name/$filename ================\n" >> $_config_file
+    cat $f >> $_config_file;
+done
