80dfd273b2749c67d30052dca84ed713cae547cd
enix403 <fog.code000@gmail.com>
2021-09-21 15:49:20 +0500

    Some big rofi ricing

---
+++ rofi/launchers/misc/launcher.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Available Styles
+#
+# blurry	blurry_full		kde_simplemenu		kde_krunner		launchpad
+# gnome_do	slingshot		appdrawer			appdrawer_alt	appfolder
+# column	row				row_center			screen			row_dock		row_dropdown
+
+# theme="row"
+# theme="row_center"
+theme="kde_simplemenu"
+
+dir="$HOME/.config/rofi/launchers/misc"
+rofi -no-lazy-grab -modi "drun,run,window" -show drun -theme $dir/"$theme"
