db79682634df512953c749afe2a6d4ea462b8000
enix403 <fog.code000@gmail.com>
2023-12-17 12:17:59 +0500

    Update

--- oh-my-zsh-custom/paths.zsh
+++ oh-my-zsh-custom/paths.zsh
@@ -1,22 +1,27 @@
+# Random
 export PATH="$HOME/.ghcup/bin:$PATH"
 export PATH="$PATH:$HOME/.yarn/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="$PATH:$HOME/.emacs.d/bin"
 
+# Golang
 export GOBIN="$HOME/go/bin"
 export PATH="$PATH:$GOBIN"
 
+# Jetbtrains Toolbox
 export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts"
 
-# export PATH="$PATH:$HOME/Applications/Sdk/flutter/bin"
-
-# export ANDROID_HOME="$HOME/Android/Sdk"
-## export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk-bundle
-# export JAVA_HOME="/usr/lib/jvm/java-11-openjdk"
+# JAVA
 export JAVA_HOME="/usr/lib/jvm/java-17-openjdk"
 
-# export PATH=${ANDROID_HOME}/emulator:${PATH}
-# export PATH=${ANDROID_HOME}/tools:${PATH}
-# export PATH=${ANDROID_HOME}/platform-tools:${PATH}
+# Flutter
+export PATH="$PATH:$HOME/Applications/flutter/bin"
+
+# Android
+export ANDROID_HOME="$HOME/Android/Sdk"
+# export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk-bundle
+export PATH=${ANDROID_HOME}/emulator:${PATH}
+export PATH=${ANDROID_HOME}/tools:${PATH}
+export PATH=${ANDROID_HOME}/platform-tools:${PATH}
 
