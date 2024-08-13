dc4d6a53c85977d840e541d5c6ea1fbd9242abf7
enix403 <fog.code000@gmail.com>
2024-08-13 15:40:17 +0500

    changes

--- nvim/lua/theradium/plugins/autoclose.lua
+++ nvim/lua/theradium/plugins/autoclose.lua
@@ -2,7 +2,7 @@ require("autoclose").setup({
   options = {
     pair_spaces = true,
     disable_when_touch = true,
-    touch_regex = "[%w!@#%$%%%^&*()%[%]{}_%-+=|\\\"';:<,>.?/`~]"
+    touch_regex = "[%w(%[{!@#%$%%%^&%*%-+=|\\:;\"'<,>.?/]"
   }
 })
 
