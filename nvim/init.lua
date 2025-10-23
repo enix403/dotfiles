bcd3239a5a2f35a134a9338743a5c0e8e7f2fd06
enix403 <fog.code000@gmail.com>
2025-10-23 19:56:58 +0500

    rename

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,28 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+telescope_builtin = require("telescope.builtin")
+
+require("theradium.packer")
+
+require("theradium.core.options")
+require("theradium.core.keymaps")
+require("theradium.core.colorscheme")
+require("theradium.core.commands")
+
+require("theradium.plugins.telescope")
+require("theradium.plugins.transparent")
+require("theradium.plugins.treesitter")
+require("theradium.plugins.lualine")
+require("theradium.plugins.indentline")
+require("theradium.plugins.nvim-tree")
+
+-- require("theradium.plugins.mason")
+-- require("theradium.plugins.lsp.servers")
+require("theradium.plugins.nvim-cmp")
+require("theradium.plugins.comment")
+require("theradium.plugins.dressing")
+require("theradium.plugins.autoclose")
+require("theradium.plugins.barbar")
+require("theradium.plugins.toggleterm")
+
+require("theradium.plugins.other")
+
+-- require("theradium.plugins.treesitter-context")
