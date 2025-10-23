93b1071a40491437ad8c34c9fbce32a1635a3e0e
enix403 <fog.code000@gmail.com>
2025-10-23 23:06:25 +0500

    add plugins

--- nvim_new/lua/plugins/all.lua
+++ nvim_new/lua/plugins/all.lua
@@ -1,3 +1,26 @@
-return {
+--[[
+disable spell check
+]]
 
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+  },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
+
+  -- Move the floating cmdline back to bottom
+  { "folke/noice.nvim", opts = {
+    cmdline = {
+      view = "cmdline",
+    },
+  } },
+
+  --[[ Disable ]]
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
