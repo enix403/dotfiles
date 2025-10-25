5cd0e2b94bf1da654f76301850023c5faa922792
enix403 <fog.code000@gmail.com>
2025-10-25 14:27:10 +0500

    changes

--- nvim_new/lua/plugins/all.lua
+++ nvim_new/lua/plugins/all.lua
@@ -21,6 +21,16 @@ return {
     },
   } },
 
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
+
   --[[ Disable ]]
 }
-
