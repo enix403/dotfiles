a4006b8f3acf02d34bdd41c7840732d89b22fa60
enix403 <fog.code000@gmail.com>
2025-10-26 13:17:56 +0500

    Changes

--- nvim_new/lua/config/keymaps.lua
+++ nvim_new/lua/config/keymaps.lua
@@ -19,6 +19,18 @@
 -- moves away)
 -- map("i", "<CR>", "<CR> <BS>")
 
+-- BUILT INTO LAZY VIM
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking (copying) text',
+--   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+-- })
+
 local map = vim.keymap.set
 
 -- Navigate windows using Shift+hjkl
