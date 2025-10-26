ab017356b666540f87710505b6a41c106f1f02ef
enix403 <fog.code000@gmail.com>
2025-10-26 12:40:11 +0500

    Changes

--- nvim_new/lua/config/keymaps.lua
+++ nvim_new/lua/config/keymaps.lua
@@ -18,7 +18,8 @@ local map = vim.keymap.set
 -- Navigate windows using Shift+hjkl
 map("n", "<S-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<S-j>", "<C-w>j", { desc = "Move to window below" })
-map("n", "<S-k>", "<C-w>k", { desc = "Move to window above" })
+-- FIXME: K (<S-k>) is used for LSP hover action
+-- map("n", "<S-k>", "<C-w>k", { desc = "Move to window above" })
 map("n", "<S-l>", "<C-w>l", { desc = "Move to right window" })
 
 -- Resize window using Ctrl+Shift+hjkl
@@ -28,16 +29,30 @@ map("n", "<C-S-j>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
 map("n", "<C-S-k>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
 
 -- Make x and c operations not modify clipboards
-map({ "n", "x" }, "x", '"_x', opts)
-map({ "n", "x" }, "X", '"_X', opts)
-map({ "n", "x" }, "c", '"_c', opts)
-map({ "n", "x" }, "C", '"_C', opts)
+map({ "n", "x" }, "x", '"_x')
+map({ "n", "x" }, "X", '"_X')
+map({ "n", "x" }, "c", '"_c')
+map({ "n", "x" }, "C", '"_C')
 
 -- I dont want fancy combinations with > and <
 -- Just want to use them to indent/deindent
-map("n", "<", "<<", opts)
-map("n", ">", ">>", opts)
+map("n", "<", "<<")
+map("n", ">", ">>")
 
--- Make o come back to normal mode
-map("n", "o", "o <BS><Esc>", opts)
-map("n", "O", "O <BS><Esc>", opts)
+-- Make > and < work in visual mode as well
+map("x", ">", ">gv")
+map("x", "<", "<gv")
+
+-- Make o/O come back to normal mode after creating a
+-- blank line below/above
+map("n", "o", "o <BS><Esc>")
+map("n", "O", "O <BS><Esc>")
+
+-- Scroll up/down half a page and put the cursor in
+-- the middle
+--
+-- <C-u>/<C-d>: move half page up/down
+-- zz: Recenter so that the line of the cursor is in the middle
+-- 0: move the cursor to the start of the line
+map({ "n", "x" }, "{", "<C-u>zz0")
+map({ "n", "x" }, "}", "<C-d>zz0")
