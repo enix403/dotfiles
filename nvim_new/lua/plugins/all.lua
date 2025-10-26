185979d77db6e6d5ca20035ae3d0277ef92a6442
enix403 <fog.code000@gmail.com>
2025-10-26 14:04:41 +0500

    Changes

--- nvim_new/lua/plugins/all.lua
+++ nvim_new/lua/plugins/all.lua
@@ -1,6 +1,31 @@
---[[
-disable spell check
-]]
+-- Disable blink.cmp completions in comments
+-- https://github.com/Saghen/blink.cmp/discussions/564#discussioncomment-13439030
+local in_treesitter_capture = function(capture)
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if vim.api.nvim_get_mode().mode == "i" then
+    col = col - 1
+  end
+
+  local buf = vim.api.nvim_get_current_buf()
+  local get_captures_at_pos = require("vim.treesitter").get_captures_at_pos
+
+  local captures_at_cursor = vim.tbl_map(function(x)
+    return x.capture
+  end, get_captures_at_pos(buf, row - 1, col))
+
+  if vim.tbl_isempty(captures_at_cursor) then
+    return false
+  elseif type(capture) == "string" and vim.tbl_contains(captures_at_cursor, capture) then
+    return true
+  elseif type(capture) == "table" then
+    for _, v in ipairs(capture) do
+      if vim.tbl_contains(captures_at_cursor, v) then
+        return true
+      end
+    end
+  end
+  return false
+end
 
 return {
   {
@@ -15,11 +40,14 @@ return {
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
 
   -- Move the floating cmdline back to bottom
-  { "folke/noice.nvim", opts = {
-    cmdline = {
-      view = "cmdline",
-    },
-  } },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    }
+},
 
   {
     "saghen/blink.cmp",
@@ -28,22 +56,11 @@ return {
         ghost_text = {
           enabled = false,
         },
-      },
-      -- https://github.com/Saghen/blink.cmp/discussions/564#discussioncomment-11905466
-      sources = {
-        default = function()
-          local node = vim.treesitter.get_node()
-          if
-            node and vim.tbl_contains({
-              "comment", "line_comment", "comment_content", "block_comment",
-              "string_start", "string_content", "string_end"
-            }, node:type())
-          then
-            return { }
-          else
-            return {  "lsp", "path", "snippets", "buffer"  }
-          end
-        end,
+        menu = {
+          auto_show = function()
+            return not in_treesitter_capture("comment")
+          end,
+        },
       },
     },
     opts_extend = { "sources.default" },
