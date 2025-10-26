abedc47cb811652df8ff8eee251994fdd67c40f3
enix403 <fog.code000@gmail.com>
2025-10-26 21:08:22 +0500

    Changes

--- nvim_new/lua/plugins/theme.lua
+++ nvim_new/lua/plugins/theme.lua
@@ -3,6 +3,20 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      transparent_background = true,
+      -- https://github.com/catppuccin/nvim/discussions/676
+      --
+      -- The default border color between splits is barely
+      -- visible (at least) on my terminal. This changes it to
+      -- match the split border color of my terminal's (kitty)
+      -- catpuccin mocha border color
+      custom_highlights = function(colors)
+        return {
+          WinSeparator = { fg = "#B4BEFE" },
+        }
+      end,
+    },
   },
   {
     "xiyaowong/transparent.nvim",
