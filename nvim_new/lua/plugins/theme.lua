9f8911bd760d2ece3f56f9862db0de3e8cf62b2e
enix403 <fog.code000@gmail.com>
2025-10-27 23:55:06 +0500

    changes

--- nvim_new/lua/plugins/theme.lua
+++ nvim_new/lua/plugins/theme.lua
@@ -4,10 +4,13 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-      transparent_background = true,
+      -- typicode/bg.nvim handles this transparency as well
+      -- as bg color syncing
+      -- transparent_background = false,
+
       -- https://github.com/catppuccin/nvim/discussions/676
       --
-      -- The default border color between splits is barely
+      -- The default border color (MOCHA) between splits is barely
       -- visible (at least) on my terminal. This changes it to
       -- match the split border color of my terminal's (kitty)
       -- catpuccin mocha border color
@@ -18,9 +21,10 @@ return {
       end,
     },
   },
-  {
-    "xiyaowong/transparent.nvim",
-    lazy = false,
-  },
+  -- {
+  --   "xiyaowong/transparent.nvim",
+  --   lazy = false,
+  --   enabled = false,
+  -- },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
 }
