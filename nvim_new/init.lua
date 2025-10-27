89b0bb9e0304bb749e0992fd6bc79ded10d3034d
enix403 <fog.code000@gmail.com>
2025-10-27 23:41:31 +0500

    changes

--- nvim_new/init.lua
+++ nvim_new/init.lua
@@ -1,2 +1,17 @@
+-- https://sw.kovidgoyal.net/kitty/mapping/#conditional-mappings-depending-on-the-state-of-the-focused-window
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+  group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+  callback = function()
+      io.stdout:write("\x1b]1337;SetUserVar=in_neovim=MQo\007")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+  group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+  callback = function()
+      io.stdout:write("\x1b]1337;SetUserVar=in_neovim\007")
+  end,
+})
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
