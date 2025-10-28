c6bc9c108852afd37d51960bdc6f3b0b28ba7778
enix403 <fog.code000@gmail.com>
2025-10-28 19:04:48 +0500

    Changes

--- nvim_new/lua/plugins/snacks.lua
+++ nvim_new/lua/plugins/snacks.lua
@@ -1,15 +1,29 @@
 return {
   "folke/snacks.nvim",
   -- opts.picker.sources.files.hidden = true
-  opts = {
-    picker = {
-      sources = {
-        files = {
-          hidden = true,
-        },
+  -- https://github.com/folke/snacks.nvim/discussions/1322#discussioncomment-14196635
+  opts = function(_, opts)
+    local sources = opts.picker.sources or {}
+    local source_names = { "files", "explorer", "grep", "grep_word", "grep_buffers" }
+    local source_opts = {
+      hidden = true,
+      ignored = true,
+      exclude = {
+        ".DS_Store",
+        "Thumbs.db",
+        ".svn",
+        ".hg",
+        ".git",
+        "node_modules",
       },
-      hidden = true, -- for hidden files
-      ignored = true, -- for .gitignore files
-    },
-  },
+    }
+
+    for _, name in ipairs(source_names) do
+      sources[name] = source_opts
+    end
+
+    opts.picker.sources = sources
+
+    return opts
+  end,
 }
