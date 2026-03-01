ee7d699d72caf0062a2347b14fa0cef4b63f1da4
enix403 <fog.code000@gmail.com>
2026-03-01 22:32:57 +0500

    changes

--- oh-my-zsh-custom/paths.zsh
+++ oh-my-zsh-custom/paths.zsh
@@ -1,14 +1,12 @@
 # Note: These are *prepened* to the current path, so the last one take priority
 export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/probin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="/usr/local/bin/override:$PATH"
 
 # Less important with low chance of ambigiguity. These are appended.
 # Order matters less
 export PATH="$PATH:$HOME/.cargo/bin"
-
-# TODO: how to manage with multiple python versions ?
-# export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 
 # not needed for now
 # export PATH="$HOME/.antigravity/antigravity/bin:$PATH"
@@ -25,7 +23,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 # -----------
 # rbenv
 
-eval "$(rbenv init - --no-rehash zsh)"
+# eval "$(rbenv init - --no-rehash zsh)"
 
 # ---------
 # Mise
