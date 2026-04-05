7d4555255e40744fa0f4ffb47c286e528eb65ce3
enix403 <fog.code000@gmail.com>
2026-04-05 17:59:09 +0500

    changes

--- linux/fonts/fonts.sh
+++ linux/fonts/fonts.sh
@@ -4,7 +4,11 @@
 
 mkd -p ~/.local/share/fonts
 
-# ======= FiraCode Nerd Font ======
+# ======= Fira Code ======
+
+# comes pre-installed with fedora
+
+# ======= Fira Code Nerd Font ======
 
 cd /tmp
 # replace with latest link
@@ -12,3 +16,11 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.z
 unzip FiraCode.zip -d ~/.local/share/fonts/custom_nerd_firacode
 fc-cache -fv
 rm FiraCode.zip
+
+# ======= Maple Mono ======
+
+cd /tmp
+# replace with latest link
+wget https://github.com/subframe7536/maple-font/releases/download/v7.9/MapleMono-TTF-AutoHint.zip
+unzip MapleMono-TTF-AutoHint.zip -d ~/.local/share/fonts/custom_maplemono
+rm MapleMono-TTF-AutoHint.zip
