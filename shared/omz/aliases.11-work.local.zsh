3af61498fbdf3fa8cb5da37c14fce5b07edab3a4
enix403 <fog.code000@gmail.com>
2026-04-05 16:46:58 +0500

    changes

--- shared/omz/aliases.11-work.local.zsh
+++
@@ -1,7 +0,0 @@
-# TODO: is this optimal ?
-# the idea is I do not want direnv's messing with my current python selection
-alias py="$HOME/.pyenv/shims/python"
-alias pip="$HOME/.pyenv/shims/pip"
-alias ipy="$HOME/.pyenv/shims/ipython"
-alias jl="$HOME/.pyenv/shims/jupyter-lab"
-alias jconv="$HOME/.pyenv/shims/jupyter nbconvert --to script"
